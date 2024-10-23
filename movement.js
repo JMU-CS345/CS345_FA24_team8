fbd112af2bfbc03ad141ca7b0463047109eadb7c
Bolanos540 <bolan2cj@dukes.jmu.edu>
2024-10-23 11:07:10 -0400

    Update movement.js
    increased player speed

--- movement.js
+++ movement.js
@@ -13,13 +13,13 @@ function draw() {
   background(220);
 
   if (keyIsDown(UP_ARROW) || keyIsDown(87)) {
-    y -= 1;
+    y -= 1.5;
   } else if (keyIsDown(LEFT_ARROW) || keyIsDown(65)) {
-    x -= 1;
+    x -= 1.5;
   } else if (keyIsDown(DOWN_ARROW) || keyIsDown(83)) {
-    y += 1;
+    y += 1.5;
   } else if (keyIsDown(RIGHT_ARROW) || keyIsDown(68)) {
-    x += 1;
+    x += 1.5;
   }
  
 
